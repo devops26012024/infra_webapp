17f1c4587a51ae6a066f91f0b1355ef5c106c854
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 14:36:09 +0530

    Update workspace.tf

--- terraform/workspace.tf
+++ terraform/workspace.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "pradeep-terraform-1234333"
-    key            = "lambda/terraform.tfstate"
+    key            = "lambda1/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-demo"
