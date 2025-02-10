153d69589ffb7f243b82938cd084bf701e51c445
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 10:26:05 +0530

    Create oidc.tf

---
+++ terraform/oidc.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "GitHubActionsRole-${terraform.workspace}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::970547381233:oidc-provider/token.actions.githubusercontent.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringLike": {
+          "token.actions.githubusercontent.com:sub": "repo:Devendraappa/*"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+output "role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
