40061eb18e7b9dd8e783b8252633f0aee6f01f39
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 10:08:46 +0530

    Create lambda.tf

---
+++ terraform/lambda.tf
@@ -0,0 +1,21 @@
+variable "lambda_configs" {
+  type = map(object({
+    memory_size = number
+    timeout     = number
+  }))
+  default = {
+    dev  = { memory_size = 128, timeout = 5 }
+    prod = { memory_size = 512, timeout = 15 }
+  }
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello-world-${terraform.workspace}"
+  role          = aws_iam_role.github_actions_role.arn
+  runtime       = "nodejs18.x"
+  handler       = "index.handler"
+  filename      = "lambda.zip"
+
+  memory_size = lookup(var.lambda_configs, terraform.workspace, var.lambda_configs["dev"]).memory_size
+  timeout     = lookup(var.lambda_configs, terraform.workspace, var.lambda_configs["dev"]).timeout
+}
