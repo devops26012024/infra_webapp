411ed7c1b7d774299a147f95d3e251cbad6ad2ef
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 12:25:13 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,6 +16,7 @@ variable "dynamodb_table" {
   default     = "terraform-lock"
 }
 
+/*
 variable "lambda_configs" {
   description = "Configuration for Lambda function"
   type = map(object({
@@ -27,11 +28,12 @@ variable "lambda_configs" {
     prod = { memory_size = 512, timeout = 15 }
   }
 }
+*/
 
 variable "github_org" {
   description = "GitHub organization for OIDC authentication"
   type        = string
-  default     = "Devendraappa"
+  default     = "devops26012024"
 }
 
 variable "environment" {
