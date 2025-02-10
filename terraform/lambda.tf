bfb6fec6e8be7eef2e11e177662c81ba3df666d2
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 14:26:30 +0530

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,7 +10,7 @@ variable "lambda_configs" {
 
 resource "aws_lambda_function" "hello_world" { 
   function_name = "hello-world-${terraform.workspace}"
-  role          = "arn:aws:iam::970547381233:role/oidc"
+  role          = "aws_iam_role.github_actions_role.arn"
   runtime       = "nodejs18.x"
   handler       = "index.handler"
   filename      = "lambda.zip"
