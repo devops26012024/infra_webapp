7291c422512f0ea367c706b969a57d9a8fa89000
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 11:30:30 +0530

    Update oidc.tf

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -3,21 +3,28 @@ resource "aws_iam_role" "github_actions_role" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Federated": "arn:aws:iam::970547381233:oidc-provider/token.actions.githubusercontent.com"
-      },
-      "Action": "sts:AssumeRoleWithWebIdentity",
-      "Condition": {
-        "StringLike": {
-          "token.actions.githubusercontent.com:sub": "repo:devops26012024/*"
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Principal": {
+                "Federated": "arn:aws:iam::970547381233:oidc-provider/token.actions.githubusercontent.com"
+            },
+            "Condition": {
+                "StringEquals": {
+                    "token.actions.githubusercontent.com:aud": [
+                        "sts.amazonaws.com"
+                    ]
+                },
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": [
+                        "repo:devops26012024/*"
+                    ]
+                }
+            }
         }
-      }
-    }
-  ]
+    ]
 }
 EOF
 }
