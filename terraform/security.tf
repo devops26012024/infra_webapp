c807cb92adb5dd5035df130652497f97a9e386cd
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 16:07:50 +0530

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,6 +1,12 @@
-resource "aws_s3_bucket" "logs_bucket-p1p2" {
-  bucket = "lambda-logs-${terraform.workspace}"
+resource "aws_s3_bucket" "logs_bucket" {
+  bucket = "lambda-logs-${random_string.suffix.result}-${terraform.workspace}"
   acl    = "private"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
 }
 
 resource "aws_cloudwatch_log_group" "lambda_logs" {
