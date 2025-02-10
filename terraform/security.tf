0983a96830ad5a6c58e49219be3df67e143c6f55
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 10:30:52 +0530

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "logs_bucket" {
+  bucket = "lambda-logs-${terraform.workspace}"
+  acl    = "private"
+}
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+  retention_in_days = 14
+}
