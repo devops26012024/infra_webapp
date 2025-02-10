37f7aef64f5beba8eaaa7d7f10673a80a5a4faa8
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-10 16:01:05 +0530

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,6 +1,7 @@
 data "aws_s3_bucket" "pradeep-terraform-1234333" {
   bucket = "lambda-logs-${terraform.workspace}"
   #acl    = "private"
+  provider = ap-south-1
 }
 
 resource "aws_cloudwatch_log_group" "lambda_logs" {
