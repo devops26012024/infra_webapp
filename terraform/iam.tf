ce0c3190d5260910dd130a05cb187cd8cb725002
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-11 13:56:34 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-role"
 
@@ -21,3 +22,59 @@ resource "aws_iam_policy_attachment" "lambda_execution" {
   roles      = [aws_iam_role.github_actions_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+*/
+
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_api_gateway_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+# IAM Policy for Lambda Logging
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:logs:*:*:*"
+    }
+  ]
+}
+EOF
+}
+
+
+
+# Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+
