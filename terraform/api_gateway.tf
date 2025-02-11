24ff8a3354a4a9430f04139e7b97be2973826748
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-11 13:29:05 +0530

    Update api_gateway.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -9,39 +9,15 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_uri    = aws_lambda_function.hello_world.invoke_arn
 }
 
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
 
 
-/*
+
+
 resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "GET /"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
-*/
 
-# GET Route
-resource "aws_apigatewayv2_route" "get_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "GET /"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
 
-# POST Route
-resource "aws_apigatewayv2_route" "post_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "POST /"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
 
-resource "aws_apigatewayv2_deployment" "lambda_deployment" {
-  api_id = aws_apigatewayv2_api.http_api.id
-  depends_on = [
-    aws_apigatewayv2_route.get_route,
-    aws_apigatewayv2_route.post_route
-  ]
-}
