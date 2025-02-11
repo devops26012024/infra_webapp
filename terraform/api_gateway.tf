23899b78fd219c0d541001aab7aa36d145f1e6c3
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-11 10:43:29 +0530

    Update api_gateway.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -9,9 +9,24 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_uri    = aws_lambda_function.hello_world.invoke_arn
 }
 
+/*
 resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "GET /"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
+*/
 
+# GET Route
+resource "aws_apigatewayv2_route" "get_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# POST Route
+resource "aws_apigatewayv2_route" "post_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
