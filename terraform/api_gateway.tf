82d062d29829dff20fffcfb434ec7e8f9028fc9b
Pradeep <160380277+pkumard4@users.noreply.github.com>
2025-02-11 14:02:46 +0530

    Update api_gateway.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -10,14 +10,27 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 }
 
 
-
-
-
 resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "GET /"
+  route_key = "GET /hello-world"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
+
+# API Gateway Stage
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Lambda Permission to API Gateway
+resource "aws_lambda_permission" "apigw_lambda" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
 
 
